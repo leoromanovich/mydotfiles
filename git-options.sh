20f9fdc571db377e8b0865af36d7425afec8206c
Leonid Verkhovtsev <leoromanovich@gmail.com>
2022-08-31 10:22:56 +0300

    add aliases and global options for git

--- git-options.sh
+++ git-options.sh
@@ -1,1 +1,9 @@
 git config --global rerere.enabled true
+git config --global alias.co checkout
+git config --global alias.ci commit
+git config --global alias.st 'status -u'
+git config --global alias.br branch
+git config --global alias.unstage 'reset HEAD --'
+git config --global alias.last 'log -1 HEAD'
+git config --global alias.lgraph 'log --pretty=format:"%h %an | %s" --graph'
+git config --global core.editor nvim
