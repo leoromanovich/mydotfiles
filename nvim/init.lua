bf0768bed23858b2f99fcaad3489bc204527b71a
Leonid Verkhovtsev <leoromanovich@gmail.com>
2025-10-06 15:24:06 +0300

    upd

--- nvim/init.lua
+++ nvim/init.lua
@@ -218,7 +218,31 @@ local plugins = {
       })
     end
   },
+    {
+      "stevearc/conform.nvim",
+      event = "VeryLazy",
+      config = function()
+        require("conform").setup({
+          -- Запускаем сначала autofix (импорты и пр.), затем форматтер
+          formatters_by_ft = {
+            python = { "ruff_fix", "ruff_format" },
+          },
+          notify_on_error = true,
+        })
 
+        -- Форматировать файл или выделение Ruff-ом
+        vim.keymap.set({ "n", "v" }, "<leader>rf", function()
+          require("conform").format({
+            async = true,
+            lsp_format = "never", -- не дергать LSP-форматирование, только Ruff
+            timeout_ms = 2000,
+          })
+        end, { desc = "Ruff: форматировать файл/выделение" })
+
+        -- (необязательно) сделать gq умным — будет использовать conform для range-format
+        -- vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+      end,
+    },
    -- Установка lazydev.nvim для автодополнения при редактировании конфигов Neovim
   {
     "folke/lazydev.nvim",
