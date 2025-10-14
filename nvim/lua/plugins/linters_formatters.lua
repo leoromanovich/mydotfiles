7692427d0282e5fbcdf3334a1d18d0225f402e27
Leonid Verkhovtsev <leoromanovich@gmail.com>
2025-10-14 16:30:54 +0300

    change formatter to black

--- nvim/lua/plugins/linters_formatters.lua
+++ nvim/lua/plugins/linters_formatters.lua
@@ -1,27 +1,42 @@
 return {
-    {
-      "stevearc/conform.nvim",
-      event = "VeryLazy",
-      config = function()
-        require("conform").setup({
-          -- Запускаем сначала autofix (импорты и пр.), затем форматтер
-          formatters_by_ft = {
-            python = { "ruff_fix", "ruff_format" },
-          },
-          notify_on_error = true,
+  {
+    "stevearc/conform.nvim",
+    event = "VeryLazy",
+    config = function()
+      require("conform").setup({
+        -- Последовательность для Python: сначала импорты Ruff, затем формат от Black
+        formatters_by_ft = {
+          python = { "ruff_fix", "black" },
+        },
+        -- Настройка самих форматтеров
+        formatters = {
+          -- Ограничим ruff_fix правилами импорта (I*)
+          ruff_fix = { prepend_args = { "--select", "I" } },
+          -- Добавь/убери --fast по вкусу
+          black = { prepend_args = { "--fast" } },
+        },
+        notify_on_error = true,
+      })
+
+      -- Горячая клавиша: форматировать файл/выделение (Ruff импорты → Black)
+      vim.keymap.set({ "n", "v" }, "<leader>rf", function()
+        require("conform").format({
+          async = true,
+          lsp_format = "never",
+          timeout_ms = 2000,
         })
+      end, { desc = "Ruff+Black: форматировать файл/выделение" })
 
-        -- Форматировать файл или выделение Ruff-ом
-        vim.keymap.set({ "n", "v" }, "<leader>rf", function()
-          require("conform").format({
-            async = true,
-            lsp_format = "never", -- не дергать LSP-форматирование, только Ruff
-            timeout_ms = 2000,
-          })
-        end, { desc = "Ruff: форматировать файл/выделение" })
+      -- По желанию: включить умный gq через conform
+      -- vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
 
-        -- (необязательно) сделать gq умным — будет использовать conform для range-format
-        -- vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
-      end,
-    },
+      -- По желанию: автоформат при сохранении
+      -- vim.api.nvim_create_autocmd("BufWritePre", {
+      --   pattern = "*.py",
+      --   callback = function()
+      --     require("conform").format({ lsp_format = "never", timeout_ms = 2000 })
+      --   end,
+      -- })
+    end,
+  },
 }
